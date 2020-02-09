ffbedc3e2ad4aadbd350d352a69555afc3c9663b
Trung Vu <trungvu.inside@gmail.com>
2020-02-09 13:52:02 +0900

    Correct the syntax

--- hack/build-base.sh
+++ hack/build-base.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-BASE_TAG = $(git rev-parse --short HEAD)
-IMAGE_NAME = chariot9/golang-infra
-APP_NAME = golang-infra
-GOLANG_TAG = 1.9.2-alpine
+BASE_TAG=$(git rev-parse --short HEAD)
+IMAGE_NAME=chariot9/golang-infra
+APP_NAME=golang-infra
+GOLANG_TAG=1.9.2-alpine
 
-BUILD_BASE_ARGS = --build-arg APP_NAME=$(APP_NAME) --build-arg GOLANG_TAG=$(GOLANG_TAG)
+BUILD_BASE_ARGS=--build-arg APP_NAME=${APP_NAME} --build-arg GOLANG_TAG=${GOLANG_TAG}
 
 echo "Building base image"
-echo $(BASE_TAG)
+echo ${BASE_TAG}
